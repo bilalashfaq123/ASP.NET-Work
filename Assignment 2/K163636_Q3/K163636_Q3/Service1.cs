087fb113e3b9e4f3ac2dce3d11e235951cbeed63
Muahmmad Bilal Ashfaq <bilal75210@gmail.com>
2020-04-27 18:53:02 +0500

    Question 3 progress

--- Assignment 2/K163636_Q3/K163636_Q3/Service1.cs
+++ Assignment 2/K163636_Q3/K163636_Q3/Service1.cs
@@ -11,6 +11,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Timers;
 using System.Xml;
+using Newtonsoft.Json;
 
 namespace K163636_Q3
 {
@@ -32,45 +33,89 @@ namespace K163636_Q3
 
         protected override void OnStart(string[] args)
         {
-            timer.Elapsed += new ElapsedEventHandler(timer1_Elapsed);
-            timer.Interval = 1000; // 1000 ms => 1 second
+            timer.Interval = _timerValue;
+            timer.AutoReset = true;
             timer.Enabled = true;
+            timer.Start();
+            timer.Elapsed += new System.Timers.ElapsedEventHandler(timer1_Elapsed);
         }
 
         protected override void OnStop()
         {
             timer.Enabled = false;
+            timer.Stop();
         }
-        private int lastminute = -1;
+
         private void timer1_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
         {
-            var curTime = DateTime.Now; // Get current time
-            if (lastminute < curTime.Minute) // If now 5 min of any hour
-            {
-                lastminute = curTime.Minute + _timerValue;
+            List<string> sList = GetDirectoryInformation();
+            
 
-                work();
-                //ReadingNamesOfPatients();
-            }
-        }
-
-        public void work()
-        {
-            try
+            foreach (var dir in sList)
             {
-                var dirName = new DirectoryInfo(@"G:\Users\").GetDirectories();
-                
-            }
-            catch (Exception exception)
-            {
-                using (StreamWriter writer = File.AppendText("C:\\Users\\Bilal\\Desktop\\Q3_logs.txt"))
+                string _path = "G:\\Users\\"+dir + "\\User-Detail\\heart_rate-" + DateTime.Now.ToString("YYYY-MM-DD") + ".json";
+                if (File.Exists(_path))
                 {
-                   
-                    writer.WriteLine("exception  " + exception.Message.ToString());
+                    var json = File.ReadAllText(_path);
+                    var medicalRecords = JsonConvert.DeserializeObject<List<MedicalRecord>>(json);
 
+                    string savingPath = "G:\\Users\\" + dir + "\\User-Detail\\Complete_heart_rate.json";
+                    MedicalRecordAdd(medicalRecords,savingPath);
                 }
             }
         }
+
+
+
+
+        void MedicalRecordAdd(List<MedicalRecord> medicalRecordsList, string pathToTheFile)
+        {
+            if (File.Exists(pathToTheFile))
+            {
+                var json = File.ReadAllText(pathToTheFile);
+                var medicalRecords = JsonConvert.DeserializeObject<List<MedicalRecord>>(json);
+
+                
+                
+
+                List<MedicalRecord> NewmedicalRecords = medicalRecords.Distinct().ToList();
+                File.WriteAllText(pathToTheFile, JsonConvert.SerializeObject(NewmedicalRecords));
+            }
+            else
+            {
+                FileCreationifNotExists(pathToTheFile);
+                List<MedicalRecord> medicalRecords = new List<MedicalRecord>();
+
+                foreach (var medicalRecord in medicalRecordsList)
+                {
+                    medicalRecords.Add(medicalRecord);
+                }
+
+                File.WriteAllText(pathToTheFile, JsonConvert.SerializeObject(medicalRecords));
+            }
+        }
+
+        private void FileCreationifNotExists(string path)
+        {
+            if (!File.Exists(path))
+            {
+                File.Create(path).Dispose();
+            }
+        }
+
+        public List<string> GetDirectoryInformation()
+        {
+            var dirName = new DirectoryInfo(@"G:\Users\").GetDirectories();
+            List<string> sList = new List<string>();
+            foreach (var dir in dirName)
+            {
+                sList.Add(dir.Name);
+            }
+
+            return sList;
+        }
+
+
         //wont need it, because better solution is to read all directory names
         public void ReadingNamesOfPatients()
         {
@@ -85,7 +130,7 @@ namespace K163636_Q3
 
                 XmlDocument xmlDocument = new XmlDocument();
                 xmlDocument.Load(_path);
-                
+
                 List<String> patientsNames = new List<String>();
 
                 foreach (XmlNode node in xmlDocument.DocumentElement)
@@ -96,8 +141,6 @@ namespace K163636_Q3
                         writer.WriteLine(name);
                     }
                 }
-
-               
             }
             catch (Exception exception)
             {
