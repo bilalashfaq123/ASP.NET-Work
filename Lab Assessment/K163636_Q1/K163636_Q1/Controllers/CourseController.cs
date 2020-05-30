543b799a137e2c78f808430e152665b0823d48e4
Muahmmad Bilal Ashfaq <bilal75210@gmail.com>
2020-05-30 16:00:30 +0500

    kk

--- Lab Assessment/K163636_Q1/K163636_Q1/Controllers/CourseController.cs
+++ Lab Assessment/K163636_Q1/K163636_Q1/Controllers/CourseController.cs
@@ -1,15 +1,54 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
+using Firebase.Storage;
 using K163636_Q1.Models;
 
 namespace K163636_Q1.Controllers
 {
     public class CourseController : ApiController
     {
+
+
+        public async Task<string> GetFileUpload()
+        {
+            var stream = new FileStream("C:\\Users\\Bilal\\Desktop\\Zoom Meeting.png", FileMode.Open);
+
+            // Construct FirebaseStorage with path to where you want to upload the file and put it there
+            var task = new FirebaseStorage("xdadeveloperes.appspot.com")
+                .Child("data")
+                .Child("random")
+                .Child("file.png")
+                .PutAsync(stream);
+
+            // Await the task to wait until upload is completed and get the download url
+
+            var downloadurl = await task;
+            return downloadurl;
+        }
+        public async Task<string> GetFileUpload(string id)
+        {
+            var stream = new FileStream(id, FileMode.Open);
+
+            // Construct FirebaseStorage with path to where you want to upload the file and put it there
+            var task = new FirebaseStorage("xdadeveloperes.appspot.com")
+                .Child("data")
+                .Child("random")
+                .Child("file.png")
+                .PutAsync(stream);
+
+            // Await the task to wait until upload is completed and get the download url
+
+            var downloadurl = await task;
+            return downloadurl;
+        }
+
+
         List<Course> Courses = new List<Course>
         {
             new Course(105,"English 3",113,3,"Something"),
@@ -41,15 +80,17 @@ namespace K163636_Q1.Controllers
             return Courses.Where(item => item.NumberOfStudentsEnrolled < students);
         }
 
+        [System.Web.Http.HttpPost]
         public void RemoveCourse(int id)
         {
             try
             {
-                Courses.Remove(Courses.Where(item => item.CourseCode == id).FirstOrDefault());
+                bool flag= Courses.Remove(Courses.FirstOrDefault(item => item.CourseCode == id));
+                ModelState.AddModelError(string.Empty, flag.ToString());
             }
             catch (Exception e)
             {
-                
+                ModelState.AddModelError(string.Empty, e.Message);
             }
         }
 
--- Lab Assessment/K163636_Q1/K163636_Q1/Controllers/HomeController.cs
+++ Lab Assessment/K163636_Q1/K163636_Q1/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,11 +9,22 @@ namespace K163636_Q1.Controllers
 {
     public class HomeController : Controller
     {
+        HttpClient httpClient = new HttpClient();
+        string baseUrl = "http://localhost:50284/";
+        
         public ActionResult Index()
         {
-            ViewBag.Title = "Home Page";
-
+            string path = "C:\\Users\\Bilal\\Desktop\\123.jpg";
+            HttpResponseMessage response = httpClient.GetAsync(baseUrl + "api2/Course/GetFileUpload?id="+path).Result;
+            string stateInfo = "";
+            if (response.IsSuccessStatusCode)
+            {
+                stateInfo = response.Content.ReadAsStringAsync().Result;
+            }
+            ViewBag.Title =  "Bilal";
+            ViewBag.State = stateInfo;
             return View();
         }
+
     }
 }
